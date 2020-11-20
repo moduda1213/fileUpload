cad9b2ca0882fbcbcd292858cf36ec140cb947f0
moduda1213 <csjh1@naver.com>
2020-11-20 16:50:43 +0900

    복습한 주석 추가

--- fileuploadtest/src/main/java/gd/fintech/fileuploadtest/service/BoardService.java
+++ fileuploadtest/src/main/java/gd/fintech/fileuploadtest/service/BoardService.java
@@ -33,7 +33,8 @@ public class BoardService {
 		
 		//1. board db 입력 -> key값 받음
 		
-		List<Boardfile> boardfile = null;
+		List<Boardfile> boardfile = null; // BoardForm 데이터 타입에 List<MultipartFile> boardfile-> 불러올 파일이 여러개일 수 있기 때문에
+		
 		if(boardForm.getBoardfile() != null) {
 			boardfile = new ArrayList<Boardfile>();
 			
@@ -41,12 +42,12 @@ public class BoardService {
 			
 			for(MultipartFile mf : boardForm.getBoardfile()) {
 				Boardfile bf = new Boardfile();
-				bf.setBoardId(board.getBoardId());
+				bf.setBoardId(board.getBoardId()); // select Key로 받아온 id
 				
-				int p = mf.getOriginalFilename().lastIndexOf(".");
-				String ext = mf.getOriginalFilename().substring(p).toLowerCase();
-				String filename = UUID.randomUUID().toString().replace("-", "");
-				
+				int p = mf.getOriginalFilename().lastIndexOf("."); //aaa.txt -> .의 위치 번호 = int
+				String ext = mf.getOriginalFilename().substring(p).toLowerCase(); // '.txt' 저장
+				String filename = UUID.randomUUID().toString().replace("-", "");  // UUID를 사용하면 가독에 어렵게 ㅁㅁ-ㅁ--ㅁ A-형식으로 나오기 때문에 '-','' 제거
+				// UUID는 Universally Unique IDentifier의 약어이고 범용 고유 식별자 =>  실제 사용 상에서 중복될 가능성이 거의 없다고 인정되기 때문에 많이 사용
 				bf.setBoardfileName(filename+ext);
 				bf.setBoardfileSize((int)mf.getSize());
 				bf.setBoardfileType(mf.getContentType());
@@ -55,7 +56,7 @@ public class BoardService {
 				
 				//파일저장
 				try {
-					mf.transferTo(new File("D:\\springwork\\fileuploadtest\\src\\main\\webapp\\upload\\"+filename+ext));
+					mf.transferTo(new File("D:\\springwork\\fileuploadtest\\src\\main\\webapp\\upload\\"+filename+ext)); // 생성경로 지정
 				}catch(Exception e) {
 					e.printStackTrace();
 					throw new RuntimeException(); // @Transactional에게 RuntimeException을 던져준다.
@@ -64,7 +65,7 @@ public class BoardService {
 		}
 		if(boardfile != null) {
 			for(Boardfile bf : boardfile) {
-				boardfileMapper.insertBoardfile(bf);
+				boardfileMapper.insertBoardfile(bf); 
 			}
 			//2. boardfile db 입력
 			//3. boardfile.size() 횟수만큼 입력
